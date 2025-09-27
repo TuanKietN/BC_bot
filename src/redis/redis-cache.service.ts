70de3d5db738e08cda999ed3d0b9b7f0268bfc1c
TuanKietN <kiet.nguyenthuantuan@ncc.asia>
2025-09-27 22:31:00 +0700

    fix redis connection

--- src/redis/redis-cache.service.ts
+++ src/redis/redis-cache.service.ts
@@ -27,6 +27,8 @@ export class RedisCacheService {
       this.redis = new Redis({
          host: process.env.REDIS_HOST || 'localhost',
          port: parseInt(process.env.REDIS_PORT || '6379'),
+         username: process.env.REDIS_USERNAME,
+         password: process.env.REDIS_PASSWORD,
          maxRetriesPerRequest: null,
       });
 
